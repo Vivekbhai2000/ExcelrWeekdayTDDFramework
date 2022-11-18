917afe22ec41acc86e880939c9354881c2a62195
Vivekbhai2000 <mstpatel123@gmail.com>
2022-11-18 22:17:49 +0530

    Only DB page code

---
+++ src/test/java/com/excelr/testcases/DashboardPageTest.java
@@ -0,0 +1,13 @@
+package com.excelr.testcases;
+
+import org.testng.annotations.Test;
+
+public class DashboardPageTest {
+	
+	@Test
+	public void ValidatedashboardPageTitle() {
+		System.out.println("M1");
+		
+	}
+
+}
